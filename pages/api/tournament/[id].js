6ea44222a3ec5eb9f6550d31775be56f49076845
Max Michaels <max@Maxs-MacBook-Air.local>
2022-11-12 13:45:58 -0500

    add error handling and tournament handling

--- pages/api/tournament/[id].js
+++ pages/api/tournament/[id].js
@@ -1,16 +1,23 @@
 export default async function handler(req, res) {
 
     const matchPlayBase = "https://matchplay.events/data/tournaments/";
+    const acceptableResponses = [200, 304];
     
     const { id } = req.query;
     const getLeagueData = async () => {
         const response = await fetch(matchPlayBase + id);
+        if (! acceptableResponses.includes(response.status) ){
+            return "hrmm, something went wrong, is your ID right?";
+        }
         const jsonData = await response.json();
         return jsonData.name;
     };
   
     const getPlayerData = async () => {
         const response = await fetch(matchPlayBase + id + "/standings");
+        if (! acceptableResponses.includes(response.status) ){
+            return [{name: "", position: "", points: ""}];
+        }
         const jsonData = await response.json();
         return jsonData;
     };
